2a4cd76b76e0dc59e9de9c542b7ddd4a0c5d792e
Peyang <peyang@peya.tokyo>
2022-10-25 23:59:06 +0900

    feat: uninstall hook

--- src/main/java/net/kunmc/lab/kpmtester/TestHook.java
+++ src/main/java/net/kunmc/lab/kpmtester/TestHook.java
@@ -3,7 +3,8 @@ package net.kunmc.lab.kpmtester;
 import net.kunmc.lab.kpm.KPMDaemon;
 import net.kunmc.lab.kpm.hook.HookListener;
 import net.kunmc.lab.kpm.hook.KPMHookRecipient;
-import net.kunmc.lab.kpm.hook.hooks.PluginInstallHook;
+import net.kunmc.lab.kpm.hook.hooks.PluginInstalledHook;
+import net.kunmc.lab.kpm.hook.hooks.PluginUninstallHook;
 
 public class TestHook extends KPMHookRecipient
 {
@@ -13,10 +14,25 @@ public class TestHook extends KPMHookRecipient
     }
 
     @HookListener
-    public void onInstall(PluginInstallHook hook)
+    public void onInstall(PluginInstalledHook hook)
     {
         System.out.println("Install fired!");
         System.out.println("Query: " + hook.getResolveQuery());
         System.out.println("Operator: " + hook.getOperator());
     }
+
+    @HookListener
+    public void onUninstallPre(PluginUninstallHook.Pre hook)
+    {
+        System.out.println("Uninstall.Pre fired!");
+        System.out.println("Plugin: " + hook.getPlugin());
+    }
+
+    @HookListener
+    public void onUninstallPost(PluginUninstallHook.Post hook)
+    {
+        System.out.println("Uninstall.Post fired!");
+        System.out.println("Plugin: " + hook.getPluginDescription());
+    }
+
 }
